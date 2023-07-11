4887c8b4c791e29afbf58be6f92c114b0f15e91b
Phil Nash <phil.nash@github.com>
2023-07-11 15:29:44 +0100

    Swapped 32 bit detection and added warnings

--- platform.h
+++ platform.h
@@ -1,7 +1,9 @@
-#if UINTPTR_MAX == 0xffFFffFF // 32-bit platform
-#define ARCH_32
-#elif UINTPTR_MAX == 0xffFFffFFffFFffFF // 64-bit platform
+#if UINTPTR_MAX == 0xffFFffFFffFFffFF // 64-bit platform
 #define ARCH_64
+#warning Building for 64 bit
+#elif UINTPTR_MAX == 0xffFFffFF // 32-bit platform
+#define ARCH_32
+#warning Building for 32 bit
 #else
 #error Unknown platform - does not look either like 32-bit or 64-bit
 #endif
